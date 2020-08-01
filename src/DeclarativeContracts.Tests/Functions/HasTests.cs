ce815b44218a0c71d7c150847c68a02d1ce1620e
ArturLavrov <arturstylus@gmail.com>
2020-08-01 19:40:20 +0300

    add empty tests

---
+++ src/DeclarativeContracts.Tests/Functions/HasTests.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace DeclarativeContracts.Tests.Functions
+{
+    [TestFixture]
+    public class HasTests
+    {
+        [Test]
+        public void AtLeastOneValue_ListWithOneValue_ReturnsTrue()
+        {
+
+        }
+
+        [Test]
+        public void AtLeastOneValue_ListWithNoValue_ReturnFalse()
+        {
+
+        }
+
+        [Test]
+        public void AtLeastOneValue_Null_ReturnFalse()
+        {
+
+        }
+
+        [Test]
+        public void AtLeastOneValue_ListWithFewValues_ReturnTrue()
+        {
+
+        }
+    }
+}
