7c602532b52764295ec4c3322e9a9aea17169def
Artur Lavrov <arturstylus@gmail.com>
2021-02-27 14:54:54 +0200

    Implement and overload with custom contract violation message for postconditions

--- src/DeclarativeContracts/Postcondition/Ensure.cs
+++ src/DeclarativeContracts/Postcondition/Ensure.cs
@@ -28,6 +28,27 @@ namespace DeclarativeContracts.Postcondition
                 throw new ContractViolationException("Contact postcondition was violated. Expected that predicate returns true.");
             }
         }
+
+        /// <summary>
+        ///  Method that verify element satisfied specified predicate
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="memberSelector"></param>
+        /// <param name="predicate"></param>
+        /// <param name="contractViolationMsg">Custom text that will be throw as an exception text</param>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <typeparam name="TMember"></typeparam>
+        /// <returns></returns>
+        public static void That<TEntity, TMember>(TEntity entity, Func<TEntity, TMember> memberSelector, Predicate<TMember> predicate, string contractViolationMsg)
+        {
+            ArgumentChecker.CheckArgumentsNull(memberSelector, predicate);
+
+            var member = memberSelector.Invoke(entity);
+            if (!predicate(member))
+            {
+                throw new ContractViolationException(contractViolationMsg);
+            }
+        }
         
         /// <summary>
         /// Verify that verify element satisfied specified predicate
@@ -42,6 +63,23 @@ namespace DeclarativeContracts.Postcondition
             if (!predicate(element))
             {
                 throw new ContractViolationException("Contact postcondition was violated. Expected that predicate returns true.");
+            }
+        }
+
+        /// <summary>
+        /// Verify that verify element satisfied specified predicate
+        /// </summary>
+        /// <param name="element">Element that needs to be checked</param>
+        /// <param name="predicate">Predicate that checks selected member</param>
+        /// <param name="contractViolationMessage">Custom text that will be throw as an exception text</param>
+        /// <typeparam name="TElement">Element type to check</typeparam>
+        public static void That<TElement>(TElement element, Predicate<TElement> predicate, string contractViolationMessage)
+        {
+            ArgumentChecker.CheckArgumentsNull(predicate, predicate);
+
+            if (!predicate(element))
+            {
+                throw new ContractViolationException(contractViolationMessage);
             }
         }
 
