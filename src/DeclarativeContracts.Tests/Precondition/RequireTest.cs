d52ff5803d5cf40eb6aacf4b149c33f412e6d50a
Artur Lavrov <arturstylus@gmail.com>
2021-02-27 18:39:27 +0200

    increment version and remove space from unit test

--- src/DeclarativeContracts.Tests/Precondition/RequireTest.cs
+++ src/DeclarativeContracts.Tests/Precondition/RequireTest.cs
@@ -9,9 +9,7 @@ namespace DeclarativeContracts.Tests.Precondition
     [TestFixture]
     public class RequireTest
     {
-        
-        
-        
+
         [Test]
         public void That_ValidArguemntsAndContractNotViolated_ContractViolationExceptionDoesNotThrows()
         {
